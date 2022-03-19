fc9afbf920881ddc61ca397b6ed0c0208ad1ebc1
hainisa <nisacantik@gmail.com>
2022-03-19 23:16:00 +0700

    menambahkan .gitignore,application/,composer.json,contributing.md,index.php,license.txt,readme.rst,system/,user_guide/

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya imam nawawi 
+        saya tinggal di daerah ciputat
+        olah raga yang saya sukai adalah
+        bulutangkis";
+    }
+}
