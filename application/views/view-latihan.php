747912ad398683be42efb649925f75a3cc404ec3
annisa <annisamaulina@gmail.com>
2022-03-27 17:55:35 +0700

    menambahkan .htaccess, application/controllers/Latihan1.php, application/models/Model_latihan1.php, application/views/view-latihan.php

---
+++ application/views/view-latihan.php
@@ -0,0 +1,17 @@
+<html>
+
+<head> 
+    <title> Latihan 1 </title>
+</head>
+
+<body>
+    Halo kawan..  Yuk kita belajar web programming..!!! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yauitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil?>
+
+</body>
+
+</html>
+
+
